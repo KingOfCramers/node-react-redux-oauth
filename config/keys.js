be6256bd2aca74f6c339422fc31ad65bc9529cff
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-03-26 20:31:27 -0400

    OAuth commit

--- config/keys.js
+++ config/keys.js
@@ -7,15 +7,3 @@ if (process.env.NODE_ENV === "production") {
     // return dev keys
     module.exports = require("/dev.js")
 }
-
-module.exports = {
-    googleClientID:
-        "105885109639-fm47lnml6p4t2vq6k6ooh0n9r3142a5b.apps.googleusercontent.com",
-    googleClientSecret: "td92vNE-pseZDXLMfZB9Ei_M",
-    mongoURI:
-        "mongodb://testuser:thisisthepassword@ds221609.mlab.com:21609/fakedatabase",
-    cookieKey: "aisdfuahasbaspohbbisnvljzniue"
-};
-
-190045980903-08v0007aklhsbmv3cunn6lkctq83gu7a.apps.googleusercontent.com
-U_nBsLo4VXLOiuITdCG-Fd7-
