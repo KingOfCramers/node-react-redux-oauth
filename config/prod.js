aca565179718f8d3d0a7938d823d6f27d4498ebb
Harry <32515581+KingOfCramers@users.noreply.github.com>
2018-05-05 07:42:59 -0400

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -6,17 +6,3 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 };
-
-
-/*
-module.exports = {
-    googleClientID:
-        "105885109639-fm47lnml6p4t2vq6k6ooh0n9r3142a5b.apps.googleusercontent.com",
-    googleClientSecret: "td92vNE-pseZDXLMfZB9Ei_M",
-    mongoURI:
-        "mongodb://testuser:thisisthepassword@ds221609.mlab.com:21609/fakedatabase",
-    cookieKey: "aisdfuahasbaspohbbisnvljzniue"
-};
-
-190045980903-08v0007aklhsbmv3cunn6lkctq83gu7a.apps.googleusercontent.com
-U_nBsLo4VXLOiuITdCG-Fd7-*/
